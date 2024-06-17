7c8e44c9a6a4d809efb9f5435f7acbe31ce5df3e
Ceyhun Cafer <ceyhuncafer1@gmail.com>
2024-06-16 22:42:25 -0400

    VehicleDAO and VehicleInterface added

---
+++ src/main/java/com/ps/DAO/interfaces/VehicleInt.java
@@ -0,0 +1,19 @@
+package com.ps.DAO.interfaces;
+
+import com.ps.models.Vehicle;
+import java.util.List;
+
+public interface VehicleInt {
+
+    List<Vehicle> getAllVehicles();
+
+    List<Vehicle> getVehiclesByMakeModel(String make, String model);
+
+    List<Vehicle> getVehiclesByYearRange(int startYear, int endYear);
+
+    List<Vehicle> getVehiclesByColor(String color);
+
+    int createVehicle(Vehicle vehicle);
+
+    void deleteVehicle(String vin);
+}
